436b9b748287e4868f3c78513d56decd25298b7d
Areesha Yaqoob <areeshayaqoob20@gmail.com>
2025-06-27 20:54:35 +0500

    Add files via upload

---
+++ Flash card application.py
@@ -0,0 +1,48 @@
+
+question1 = input("Enter the question:") # python wil prompt: Enter the question
+answer1 = input("Enter the answer:") 
+
+question2 = input("Enter the question:")
+answer2 = input("Enter the answer:")
+
+question3 = input("Enter the question:")
+answer3 = input("Enter the answer:")
+
+question4 = input("Enter the question:")
+answer4 = input("Enter the answer:")
+
+flashcard3 = input(question3)
+answer30 = input ("Enter the answer:") # pyhton will prompt: Enter the answer to the question
+print(answer30)
+if answer30 == answer3:
+    print("Congragulations! Your answer is correct")
+else:
+    print("Your answer is wrong, try harder next time!")
+
+flashcard2 = input(question2)
+answer20 = input ("Enter the answer:") # pyhton will prompt: Enter the answer to the question
+print(answer20)
+if answer20 == answer2:
+    print("Congragulations! Your answer is correct")
+else:
+    print("Your answer is wrong, try harder next time!")
+
+print("Correct answer:", answer2)
+
+flashcard1 = input(question1)
+answer10 = input ("Enter the answer:") # pyhton will prompt: Enter the answer to the question
+print(answer10)
+if answer10 == answer1:
+    print("Congragulations! Your answer is correct")
+else:
+    print("Your answer is wrong, try harder next time!")
+
+print("Correct answer:", answer1)
+
+flashcard4 = input( question4)
+answer40 = input ("Enter the answer:") # pyhton will prompt: Enter the answer to the question
+print(answer40)
+if answer40 == answer4:
+    print("Congragulations! Your answer is correct")
+else:
+    print("Your answer is wrong, try harder next time!")
